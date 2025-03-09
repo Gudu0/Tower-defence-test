a1edca3d0590223ba396a43819e90d4aee95c843
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-03-09 00:33:22 -0800

    Update script.js
    hopefully setup a system to get the position of my cursor

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 //Variables
 const myCanvas = document.getElementById("canvasid");
 const ctx = myCanvas.getContext("2d");
+document.addEventListener("mousemove", getmospos, false);
+const pageX = document.getElementById("posx");
+const pageY = document.getElementById("posy");
 //Draw Loop
+setInterval(draw, 20);
 
+function draw(){
+  getmospos();
+}
 //Other?
 
 //Functions
+function getmospos(){
+  pageX.innerText = "X: "+event.pageX;
+  pageY.innerText = "Y: "+event.pageY;
+}
