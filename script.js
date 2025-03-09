66f89882a3e92e6073abd2e487e0c7677ad62466
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-03-09 00:56:29 -0800

    Update script.js
    commented out the stuff in draw func

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const ctx = myCanvas.getContext("2d");
 document.addEventListener("mousemove", getmospos, false);
 const pageX = document.getElementById("posx");
 const pageY = document.getElementById("posy");
+
 //Draw Loop
 myCanvas.onload = setInterval(draw, 20);
 
 function draw(){
-  getmospos();
+  //getmospos();
 }
+
 //Other?
 
 //Functions
