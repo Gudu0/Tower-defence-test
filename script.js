abbe00b729fbbdd1d74b363bce961b7d49772dd1
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-03-09 23:37:24 -0700

    Update script.js
    added some code to put a background in the canvas

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const myCanvas = document.getElementById("canvasid");
 const ctx = myCanvas.getContext("2d");
 let rx = 0;
 let ry = 0;
+
 //Draw Loop
 setInterval(draw, 20);
 
 function draw(){
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  rectangle()
+  background();
+  rectangle();
 }
 
 //Other?
@@ -17,7 +19,7 @@ function draw(){
 function rectangle(){
   ctx.beginPath();
   ctx.rect(rx,ry,50,50);
-  ctx.fill()
+  ctx.fill();
   rx+=2;
 }
 
@@ -30,4 +32,11 @@ function showCoords(event) {
 
 function clearCoor() {
   document.getElementById("posx").innerHTML = "X";
+}
+
+function background(){
+  ctx.beginPath();
+  ctx.rect(0,0, myCanvas.width, myCanvas.height);
+  ctx.fillStyle = "#474747";
+  ctx.fill();
 }
