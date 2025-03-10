aac5a5d0b7ce630e4b6b0994beb72517e604e650
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-03-09 22:45:33 -0700

    Update script.js
    added code to draw a rectangle to the canvas, that moves.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 //Variables
 const myCanvas = document.getElementById("canvasid");
 const ctx = myCanvas.getContext("2d");
-
+let rx = 0;
+let ry = 0;
 //Draw Loop
 //myCanvas.onload = setInterval(draw, 20);
 
 function draw(){
+  ctx.beginPath();
   ctx.clearRect(0, 0, ctx.width, ctx.height);
-
+  rectangle()
 }
 
 //Other?
 
 //Functions
+function rectangle(){
+  rect(rx,ry,50,50);
+  rx+=50;
+}
