54c48f7741b00bb2d3c22ba64e85505c3b5226e7
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-03-09 18:48:35 -0700

    Update script.js
    removed some non-working code, replaced it with hopefully working code.

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 //Variables
 const myCanvas = document.getElementById("canvasid");
 const ctx = myCanvas.getContext("2d");
-document.addEventListener("mousemove", getmospos, false);
-const pageX = document.getElementById("posx");
-const pageY = document.getElementById("posy");
 
 //Draw Loop
-myCanvas.onload = setInterval(draw, 20);
+//myCanvas.onload = setInterval(draw, 20);
 
 function draw(){
-  //getmospos();
+
 }
 
 //Other?
 
 //Functions
-function getmospos(){
-  pageX.innerText = "X: " + Str(event.pageX);
-  pageY.innerText = "Y: " + Str(event.pageY);
+function showCoords(event) {
+  var x = event.pageX;
+  var y = event.pageY;
+  var coor = "X coords: " + x + ", Y coords: " + y;
+  document.getElementById("posx").innerHTML = coor;
+}
+
+function clearCoor() {
+  document.getElementById("posx").innerHTML = "";
 }
