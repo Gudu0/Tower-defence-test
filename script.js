886150b807ea200ec4e3fa220e820c4d1fd4dfff
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-03-09 22:46:25 -0700

    Update script.js
    uncommented the line for starting the draw loop

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ctx = myCanvas.getContext("2d");
 let rx = 0;
 let ry = 0;
 //Draw Loop
-//myCanvas.onload = setInterval(draw, 20);
+myCanvas.onload = setInterval(draw, 20);
 
 function draw(){
   ctx.beginPath();
