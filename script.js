59860ba508b0c7db0466d0775b9bd8cc3e983682
gudu0 <125727026+Gudu0@users.noreply.github.com>
2025-03-12 23:32:32 -0700

    Update script.js
    changed my variables to be more descriptive.

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 //Variables
 const myCanvas = document.getElementById("canvasid");
 const ctx = myCanvas.getContext("2d");
-let rx = 0; //Rectangle X
-let ry = 0; //Rectangle Y
-let singleCheck = 0; //Going to use this for something I only want to run once.
-const extra1 = document.getElementById("ex1");
+let rextangleX = 0; //Rectangle X
+let rectangleY = 0; //Rectangle Y
+const extra1 = document.getElementById("ex1");//extra p elements.
 const extra2 = document.getElementById("ex2");
 const extra3 = document.getElementById("ex3");
-let mx = 0;//Mouse X
-let my = 0;//mouse Y
-let tx = myCanvas.width / 2;
-let ty = myCanvas.height / 2;
-let ts = 10;//tower size
+let mouseX = 0; //Mouse X
+let mouseY = 0; //mouse Y
+let towerX = myCanvas.width / 2; //should be 500
+let towerY = myCanvas.height / 2; //should be 250
+let towerSize = 10; //tower size
 let ttma; //tower to mouse angle.
   
 //Draw Loop
@@ -29,8 +28,8 @@ function draw(){
 
 //Functions
 function mouseCords(event){
-  mx = event.pageX;
-  my = event.pageY;
+  mouseX = event.pageX;
+  mouseY = event.pageY;
   showCoords();
 }
 
@@ -43,29 +42,29 @@ function tower(){
   ctx.beginPath();
   ctx.save();
   //ttma = Math.atan2(my - ty, mx - tx);
-  ttma = Math.atan2(my - ty, mx - tx); //finding the angle from tower to mouse //maybe work? its from google ai overview.
-  ctx.translate(tx, ty); 
+  ttma = Math.atan2(mouseY - towerY, mouseX - towerX); //finding the angle from tower to mouse //maybe work? its from google ai overview.
+  ctx.translate(towerX, towerY); 
   ctx.rotate(ttma);
   //rect(-rectSize / 2, -rectSize / 2, rectSize, rectSize);
-  ctx.rect(-ts/2, -ts/2, ts, ts);
+  ctx.rect(-towerSize / 2, -towerSize / 2, towerSize, towerSize);
   ctx.fill();
 }
 
 function rectangle(){ //rectangle that moves across the screen
   ctx.beginPath();
-  ctx.rect(rx,ry,50,50);
+  ctx.rect(rectangleX, rectangleY, 50, 50);
   ctx.fillStyle = "#FF0000";
   ctx.fill();
-  if(rx !== myCanvas.width){
-    rx+=2;
-  } else if(rx >= myCanvas.width){
-    rx-= myCanvas.width;
-    rx-=20;
+  if(rectangleX !== myCanvas.width){
+    rectangleX += 2;
+  } else if(rectangleX >= myCanvas.width){
+    rectangleX -= myCanvas.width;
+    rectangleX -= 20;
   }
 }
 
 function showCoords() {
-  document.getElementById("posx").innerHTML = " X coords: " + mx + ", Y coords: " + my;
+  document.getElementById("posx").innerHTML = " X coords: " + mouseX + ", Y coords: " + mouseY;
 }
 
 function background(){ //Drawing the background.
